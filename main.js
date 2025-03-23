4951b51a4b2224a18963ced8084f0978d6d88fc5
JackSon <ngongocson39@gmail.com>
2025-03-23 12:12:32 +0700

    Ràng buộc password

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
-let a = prompt("Nhập 1 số nguyên a");
-while (isNaN(a) || a % 1 !== 0 || a <= 0) {
-  a = Number(prompt("Nhập lại số nguyên a "));
-}
-console.log("Số mày nhập là " + a);
-
-let sum = 0;
-for (let n = 1; n <= a; n++) {
-  let gt = 1;
-  for (let i = 1; i <= n; i++) {
-    gt *= i;
+function checkPassWord(a) {
+  let aa = false;
+  let aA = false;
+  let a1 = false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] >= "a" && a[i] <= "z") {
+      aa = true;
+    }
+    if (a[i] >= "A" && a[i] <= "Z") {
+      aA = true;
+    }
+    if (a[i] >= "0" && a[i] <= "9") {
+      a1 = true;
+    }
   }
-  sum += gt;
-  console.log("Giai thừa " + n + " là " + gt);
+  return aa && aA && a1 && a.length >= 6;
+}
+function isValidPassword(a) {
+  let hasLower = /[a-z]/.test(a);
+  let hasUpper = /[A-Z]/.test(a);
+  let hasNumber = /[0-9]/.test(a);
+  return hasLower && hasUpper && hasNumber && password.length >= 6;
+}
+let a = prompt("Mời nhập mật khẩu");
+while (!isValidPassword(a)) {
+  alert(` Vui lòng nhập mật khẩu có:
+    1. Ít nhất 1 chữ thường.
+    2. Ít nhất 1 chữ hoa.
+    3. Ít nhất 1 số.
+    4. Ít nhất 6 kí tự.
+    `);
+  a = prompt("Mời nhập lại mật khẩu");
 }
 
-console.log(sum);
+alert("Mật khẩu vừa nhập là " + a);
