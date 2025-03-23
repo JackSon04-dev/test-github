a83f76f63aa78e6e2b1ecdcaa5908b730bb1969e
JackSon <ngongocson39@gmail.com>
2025-03-23 12:50:05 +0700

    Ràng buộc password code rút gọn

--- main.js
+++ main.js
@@ -1,25 +1,9 @@
-function checkPassWord(a) {
-  let aa = false;
-  let aA = false;
-  let a1 = false;
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] >= "a" && a[i] <= "z") {
-      aa = true;
-    }
-    if (a[i] >= "A" && a[i] <= "Z") {
-      aA = true;
-    }
-    if (a[i] >= "0" && a[i] <= "9") {
-      a1 = true;
-    }
-  }
-  return aa && aA && a1 && a.length >= 6;
-}
 function isValidPassword(a) {
   let hasLower = /[a-z]/.test(a);
   let hasUpper = /[A-Z]/.test(a);
   let hasNumber = /[0-9]/.test(a);
-  return hasLower && hasUpper && hasNumber && password.length >= 6;
+  return hasLower && hasUpper && hasNumber && a.length >= 6;
+  ds;
 }
 let a = prompt("Mời nhập mật khẩu");
 while (!isValidPassword(a)) {
@@ -32,4 +16,5 @@ while (!isValidPassword(a)) {
   a = prompt("Mời nhập lại mật khẩu");
 }
 
-alert("Mật khẩu vừa nhập là " + a);
+alert("Mật khẩu là " + a);
+console.log(a);
